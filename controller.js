883fad9abe522ae29bbdd9af78ee2ca740ce3feb
Merv Arnold-Lyons <merv.arnold@gmail.com>
2014-12-03 01:22:49 -0500

    Add gender selection

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@
 
 angular.module('madLibs', [])
 	.controller('mainCtrl', function($scope){
-		$scope.female_name = 'Female name';
+		$scope.person_name = 'Person name';
 		$scope.job_title = 'Job title';
 		$scope.tedious_task = 'Tedious task';
 		$scope.dirty_task = 'Dirty task';
@@ -11,4 +11,25 @@ angular.module('madLibs', [])
 		$scope.adjective = 'Adjective';
 		$scope.obnoxious_celebrity = 'Obnoxious celebrity';
 		$scope.huge_number = 'Huge number';
+		
+		//Defaults gender to female
+		$scope.gender = 'F';
+		$scope.sPronoun = 'she';
+		$scope.pPronoun = 'her';
+		$scope.oPronoun = 'her';
+
+		$scope.$watch('gender', function(newGender, oldGender){
+			if (!newGender) return;
+			if (newGender == oldGender) return;
+			if (newGender == 'M') {
+				$scope.sPronoun = 'he';
+				$scope.pPronoun = 'his';
+				$scope.oPronoun = 'him';
+			}
+			else if (newGender == 'F') {
+				$scope.sPronoun = 'she';
+				$scope.pPronoun = 'her';
+				$scope.oPronoun = 'her';
+			}
+		});
 	});
